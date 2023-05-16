551d5acd7a5166e5ae4557d818049431ee7e084c
reptilianIX <reptlianIX@gmail.com>
2023-05-16 16:23:26 +0300

    test changes

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([2], -1) == [2]
+    assert arrs.my_slice([2, 3], -3) == [2, 3]
